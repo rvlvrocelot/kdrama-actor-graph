11aab655f21a096b3c9c91b8e1655d8328b4744c
rvlvrocelot <andrew.mahan@colorado.edu>
2015-10-21 16:23:02 -0400

    Dump drama info to Mongodb

--- actorGraph.py
+++ actorGraph.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib
 import urllib2
+from pymongo import MongoClient
 
-class parseWiki():
+class ParseWiki():
     
     rawData = {}
+    client = MongoClient()
+    drama = client.db.drama
     
     def __init__(self):
         pass
@@ -24,7 +27,7 @@ class parseWiki():
         while iter.next != last:
             if iter.string: tags += unicode(iter)
             iter = iter.next
-        return tags
+        return tags       
         
         
     def processDrama(self,dramaName):
@@ -42,12 +45,31 @@ class parseWiki():
         
     def getCast(self,dramaName):
         return self.rawData[dramaName.replace(' ','')]['cast']
+    
+    def save(self,dramaName):
+        name = dramaName.replace(' ','')
+        entry =  self.rawData[name]
+        entry['name'] = name
+        return self.drama.insert_one(entry).inserted_id
+    
+    def recover(self):
+        for x in self.drama.find():
+            print x
+    
+    def drop(self):
+        self.drama.drop()
+        
 
 if __name__ == '__main__':
 
-    parser = parseWiki()
-    parser.processDrama('A Tale of Two Sisters')
+    parser = ParseWiki()
+    parser.processDrama('Bad Couple')
     parser.processDrama('Can We Love Again?')
 
-    print parser.getCast('Can We Love Again?')
+
+    parser.drop()
+
+    parser.save('Bad Couple')
+
+    parser.recover()
 
