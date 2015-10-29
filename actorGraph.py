f371d6c1b7bfc54ab00e9b28c067551e8defa905
rvlvrocelot <andrew.mahan@colorado.edu>
2015-10-28 23:43:57 -0400

    Added get dramalist by year, error handling

--- actorGraph.py
+++ actorGraph.py
@@ -19,26 +19,47 @@ class ParseWiki():
     
     def _getBetween(self,htmlString,firstTag):
         
-        html = BeautifulSoup(htmlString,from_encoding="UTF-8")
-        first = html.find(id = firstTag)
-        last = first.find_next('span')
-        tags = ""
-        iter = first
-        while iter.next != last:
-            if iter.string: tags += unicode(iter)
-            iter = iter.next
-        return tags       
-
-       def _processCast(self,html,name):
+        try:
+            html = BeautifulSoup(htmlString,from_encoding="UTF-8")
+            first = html.find(id = firstTag)
+            last = first.find_next('span')
+            tags = ""
+            iter = first
+            while iter.next != last:
+                #if iter.string: 
+                tags += unicode(iter)
+                iter = iter.next
+        
+        except AttributeError:
+            tags = ""
+        return tags
+            
+    
+    def _processCast(self,html,name):
+        
         castSubSet = self._getBetween(html,'Cast')
-        soup = BeautifulSoup(castSubSet)
-        self.rawData[name.replace('_','')]['cast'] =  [x.get('title') for x in soup.find_all('a')]
+        
+        if not castSubSet:
+            self.rawData[name.replace('_','')]['cast'] = []
+            return 0
+        
+        soup = BeautifulSoup(html)
+        self.rawData[name.replace('_','')]['cast'] =  set([x.get('title') for x in soup.find_all('a')])
         
     def _processSynopsis(self,html,name):
-        synopsisSubSet = self._getBetween(html,'Synopsis')
-        soup = BeautifulSoup(synopsisSubSet)
-        self.rawData[name.replace('_','')]['synopsis'] = soup.find_all('p',text=True)[0].contents[0]
         
+        synopsisSubSet = self._getBetween(html,'Synopsis')
+        
+        if not synopsisSubSet:
+            self.rawData[name.replace('_','')]['synopsis'] = []
+            return 0
+        
+        soup = BeautifulSoup(synopsisSubSet)
+        try:
+            self.rawData[name.replace('_','')]['synopsis'] = soup.find_all('p')[0].get_text()
+        except IndexError:
+            self.rawData[name.replace('_','')]['synopsis'] = []
+            
         
     def processDrama(self,dramaName):
         
@@ -52,8 +73,12 @@ class ParseWiki():
         self._processCast(html,name)
         self._processSynopsis(html,name)
         
+        
     def getCast(self,dramaName):
         return self.rawData[dramaName.replace(' ','')]['cast']
+    
+    def getSynopsis(self,dramaName):
+        return self.rawData[dramaName.replace(' ','')]['synopsis']
     
     def save(self,dramaName):
         name = dramaName.replace(' ','')
@@ -72,8 +97,24 @@ class ParseWiki():
 if __name__ == '__main__':
 
     parser = ParseWiki()
-    parser.processDrama('Bad Couple')
-    parser.processDrama('Can We Love Again?')
+    def getDramaList(year):
+            html = parser._download("http://wiki.d-addicts.com/List_of_Dramas_aired_in_Korea_by_Network_in_" + str(year))
+            soup = BeautifulSoup(html)
+            rawList = [x.get('title') for x in soup.find_all('a')]
+            endString = "List of Dramas aired in Korea by Network in " + str(year -1)
+            finalList = [x for x in rawList if x]
+            finalList = finalList[0:finalList.index(endString)]
+            finalList = [x.encode("utf8") for x in finalList]
+            for i,x in enumerate(finalList):
+                if '(' in x:
+                    finalList[i] = x[0:x.index('(') -1]
+                
+            return finalList
+
+    dramaList = getDramaList(2014)
+    for drama in dramaList:
+        print drama
+        parser.processDrama(drama)
 
 
     parser.drop()
